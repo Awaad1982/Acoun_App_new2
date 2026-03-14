8d47e19471efc15a332dc69db3a351726ed2c26a
Awaad1982 <awaadawaad1982@gmail.com>
2026-03-14 22:55:37 +0300

    Initial commit: upload full Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:acoun_app_new/ItemPage.dart';
+import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+
+void main() {
+  // تهيئة SQLite على Windows/Desktop
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Item Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ItemPage(), // الصفحة الرئيسية
+    );
+  }
+}
